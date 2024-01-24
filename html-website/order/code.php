cc67df47b4f197d0d29a933253d918e27574128e
asgartutorial <157628001+asgartutorial@users.noreply.github.com>
2024-01-24 23:39:58 +0530

    Add files via upload

---
+++ html-website/order/code.php
@@ -0,0 +1,10 @@
+<?php
+    $Order_Id=$_POST['Order_Id'];
+    $age=$_POST['age'];
+    $city=$_POST['city'];
+ 
+    echo "Your Order Id Is : $Order_Id ";
+    echo "Take A Screen Short Of This Id";
+ 
+    
+?>
